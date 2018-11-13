64f747aa324551e87661edeb94c3fc31fcc77d2e
Dovydas Stirpeika <dovy91@gmail.com>
2018-11-13 17:14:51 -0600

    objFilter

--- src/object.ts
+++ src/object.ts
@@ -34,6 +34,7 @@ export function Obj<
         extract: (keys: K[]): { [P in K]: O[P]; } => objExtract(o, keys),
         removeKeys: (filterOut: K[]): { [L in Exclude<keyof O, K>]: O[L]; } => objRemoveKeys(o, filterOut),
         map: <T>(funct: (keyVal: { key: sKeys<O>; val: O[sKeys<O>]; }) => T) => objMap(o, funct),
+        filter: (funct: (keyVal: { key: sKeys<O>; val: O[sKeys<O>]; }) => boolean) => objFilter(o, funct),
         resolveObj: (path: string): any => objResolve(o, path),
     };
 };
@@ -43,9 +44,23 @@ export function objMap<
     T, O extends {}
 >(o: O, funct: (keyVal: { key: sKeys<O>, val: O[sKeys<O>] }) => T) {
     const newObj = {} as { [P in sKeys<O>]: T };
-    objKeyVals(o).forEach((keyVal) => newObj[keyVal.key] = funct(keyVal));
+
+    for (let key in o) {
+        newObj[key] = funct({ key, val: o[key] });
+    }
 
     return newObj;
+}
+
+/** Filters out keys based on bool value returned by function */
+export function objFilter<O extends {}>(o: O, funct: (keyVal: { key: sKeys<O>, val: O[sKeys<O>] }) => boolean) {
+    const newObj = { ...o as any } as { [P in sKeys<O>]?: O[P] };
+
+    for (let key in o) {
+        if (!funct({ key, val: o[key] })) delete newObj[key]
+    }
+
+    return newObj
 }
 
 /** Removes all keys from object in the `filterOut` array */
